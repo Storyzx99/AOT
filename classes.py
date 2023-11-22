9b2241e2c44706baeae36302706c207dd96be8eb
Mohammed <m.athar2@student.curtin.edu.au>
2023-11-22 18:41:13 +0400

    Fixed File

--- classes.py
+++ classes.py
@@ -1,9 +1,7 @@
 #AOT Project
 class Titan:
-    pass
-
-def __init__(self, name, health, power):
-    self.name = name
-    self.health = health
-    self.power = power
+    def __init__(self, name, health, power):
+        self.name = name
+        self.health = health
+        self.power = power
     
